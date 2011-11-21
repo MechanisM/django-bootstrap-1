933e431ab00f2a0f9101e56724c348a0cb46549a
Caio Ariede <caio.ariede@gmail.com>
2011-11-21 14:35:59 -0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 root = os.path.dirname(os.path.abspath(__file__))
 os.chdir(root)
 
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 
 # Make data go to the right place.
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
